142bb274dfa0b76d76e4c94479bb65e876f0679e
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-08-04 12:50:00 -0400

    Updated Schema and started inpu ting seed data for active-roster table

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -6,16 +6,17 @@ CREATE DATABASE globe_trotter_dev;
 
 CREATE TABLE active_roster (
     id SERIAL PRIMARY KEY,
-    position , 
-    height, 
-    nickname, 
-    hometown,
-    almamater,
-    jersey number, 
-    bio, 
-    career highlights,
-    career history,
-    favorite_quote,
-    hobbies,
-)
+    position TEXT NOT NULL, 
+    height TEXT NOT NULL, 
+    nickname TEXT NOT NULL ,
+    lastname TEXT NOT NULL,
+    hometown TEXT NOT NULL,
+    almamater TEXT,
+    jersey_number INTEGER, 
+    bio TEXT NOT NULL, 
+    career_highlights TEXT,
+    career_history TEXT,
+    favorite_quote TEXT,
+    hobbies TEXT
+);
 
