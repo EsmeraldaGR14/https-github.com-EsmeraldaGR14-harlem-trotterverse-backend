aa929bbd6cee6d310840ae966ba412175b50b82e
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-08-07 22:32:35 -0400

    Create Playerbios component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Nav/Nav";
-import Home from "./components/Home/Home";
+import Navbar from "./components/NavBar/Navbar";
+import HomePage from "./components/HomePage/HomePage";
 import AllPlayers from "./components/AllPlayers/AllPlayers";
 import EditPlayer from "./components/EditPlayer.js/EditPlayer";
 
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<HomePage />}></Route>
           <Route path="/players" element={<AllPlayers />}></Route>
           <Route path="/players/:id/edit" element={<EditPlayer />}></Route>
         </Routes>
