450d0b2d12244d54744b701c7f3bb99f6fd6b9c3
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-08-08 23:01:19 -0400

    homepage styling complete assests for images in slideshow and fonts added

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -6,7 +6,25 @@ function HomePage() {
     "The special thing is no matter where we go, we're always the home team.";
   const author = "Dizzy Grant";
   return (
-    <div className="home-page">
+    <div className="home">
+      <div className="slideshow">
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+        <div className="slide"></div>
+      </div>
       <div className="quote-container">
         <blockquote className="quote">"{quote}"</blockquote>
         <p className="author">- {author}</p>
