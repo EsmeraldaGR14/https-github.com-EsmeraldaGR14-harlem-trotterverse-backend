9433d7a218b9696e10b533a0d20831637fca2f13
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-08-09 01:30:24 -0400

    Navbar styling complete made it responsive create-a-trotter link is a button

--- frontend/src/components/NavBar/Navbar.js
+++ frontend/src/components/NavBar/Navbar.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 function Nav() {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/user-trotter">
-        <button>Create-A-Trotter</button>
-      </NavLink>
+    <nav className="navbar">
+      <div className="navbar-icon"></div>
+      <div className="navbar-links">
+        <h1 className="navbar-title">Into The TrotterVerse!</h1>
+        <NavLink className="navbar-link" to="/">
+          Home
+        </NavLink>
+        <NavLink className="navbar-link" to="/trotters">
+          Roster
+        </NavLink>
+        <NavLink className="navbar-link" to="/user-trotter">
+          <button className= "button">Create-A-Trotter</button>
+        </NavLink>
+      </div>
     </nav>
   );
 }
