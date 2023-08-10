a907c7ee8bd9b9dd2acac9afc64ab416d26480f8
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-08-10 17:03:29 -0400

    created slideshow component and map functions to dynamically display pictures in the background

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,29 +1,30 @@
 import React from "react";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 import "./HomePage.css";
 
 function HomePage() {
+  const images = [
+    "Marques_Haynes.png",
+    "teamphoto_color.png",
+    "Umbrella_bwdribble.png",
+  ];
   const quote =
     "The special thing is no matter where we go, we're always the home team.";
   const author = "Dizzy Grant";
   return (
     <div className="home">
       <div className="slideshow">
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
-        <div className="slide"></div>
+        <Slide arrows={false}>
+          {images.map((image) => (
+            <div className="each-slide-effect" key={image}>
+              <img
+                src={`${process.env.PUBLIC_URL}/images/backgroundAssets/${image}`}
+                alt=" "
+              />
+            </div>
+          ))}
+        </Slide>
       </div>
       <div className="quote-container">
         <blockquote className="quote">"{quote}"</blockquote>
