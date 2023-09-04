4d0f89cd57bbbc37480a3b8585d2295eb23a5e71
Esmeralda Gonzalez Rojo <esmeralda@Esmeraldas-Air-3792.lan>
2023-09-04 03:10:04 -0400

    updated the .env

--- frontend/src/components/Axios/Axios.js
+++ frontend/src/components/Axios/Axios.js
@@ -2,11 +2,9 @@ import axios from "axios";
 
 const AxiosInstance = axios.create({
   baseURL:
-    // process.env.NODE_ENV === "development"
-    //   ?
-    "http://localhost:3005",
-  //   :
-  //   "DEPLOY URL",
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3005"
+      : process.env.REACT_APP_API_URL,
   timeout: 50000,
 });
 
